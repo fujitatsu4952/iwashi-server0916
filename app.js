0826d187901c0014417dda4f4bd3b3e3b8b0b43f
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-09-16 23:14:23 +0900

    add

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const con = mysql.createConnection({
-  host: "iwashi0916-database.c0mbtloblzo0.ap-northeast-1.rds.amazonaws.com",
-  user: "tatsuya",
-  password: "zsEdcfTgb!1",
-  database: "iwashi_list",
+    host: 'iwashi0916-database.c0mbtloblzo0.ap-northeast-1.rds.amazonaws.com',
+    user: 'tatsuya',
+    password: 'zsEdcfTgb!1',
+    database: 'iwashi0916',
 });
 
 con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected");
+    if (err) throw err;
+    console.log('Connected');
 });
 
-app.get("/", function (req, res) {
-  res.send("Hello Worldssssss!");
+app.get('/', function (req, res) {
+    res.send('Hello Worldssssss!');
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
