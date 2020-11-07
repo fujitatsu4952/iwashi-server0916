82a8aca7412fd36df9c6800feff92fd833a7234f
fujitatsu4952 <fujitatsu49528@gmail.com>
2020-11-08 00:38:16 +0900

    add

--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
-const express = require('express');
+// const express = require('express');
+import express from "express";
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// const bodyParser = require('body-parser');
+import bodyParser from "body-parser"
+
+// const cors = require('cors');
+import cors from 'cors'
+
 app.use(bodyParser.json());
 var corsOptions = {
     exposedHeaders: 'x-auth',
